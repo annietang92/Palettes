a2f7428f14e624dc8060131c334f49ba77377512
Annie Tang <annietang92@gmail.com>
2014-05-01 14:56:27 -0400

    lala

--- app/controllers/color_controller.rb
+++ app/controllers/color_controller.rb
@@ -6,6 +6,39 @@ class ColorController < ApplicationController
 
   def show
     @color = Color.find(params[:id])
+    color = Paleta::Color.new(:hex, @color.hex)
+
+    begin
+      spectrum = Paleta::Palette.generate(:type => :shades, :from => :color, :color => color, :size => 100)
+      @spectrum = spectrum.to_array(:hex)
+      
+      analogous = Paleta::Palette.generate(:type => :analogous, :from => :color, :color => color, :size => 5)
+      @analogous = analogous.to_array(:hex)
+
+      comp = palette = Paleta::Palette.generate(:type => :complementary, :from => :color, :color => color, :size => 5)
+      @comp = comp.to_array(:hex)
+
+      split = Paleta::Palette.generate(:type => :split_complement, :from => :color, :color => color, :size => 5)
+      @split = split.to_array(:hex)
+
+      triad = Paleta::Palette.generate(:type => :triad, :from => :color, :color => color, :size => 3)
+      @triad = triad.to_array(:hex)
+
+      tetrad = palette = Paleta::Palette.generate(:type => :tetrad, :from => :color, :color => color, :size => 4)
+      @tetrad = tetrad.to_array(:hex)
+    rescue
+      if @color.is_black?
+        @gray_color = Color.find(2185)
+        gray_color = Paleta::Color.new(:hex, @gray_color.hex)
+        spectrum = Paleta::Palette.generate(:type => :shades, :from => :color, :color => gray_color, :size => 100)
+        @spectrum = spectrum.to_array(:hex)
+      else
+        @lighter_color = color.lighten!(50) 
+        lighter_color = Paleta::Color.new(:hex, @lighter_color.hex)
+        spectrum = Paleta::Palette.generate(:type => :shades, :from => :color, :color => lighter_color, :size => 100)
+        @spectrum = spectrum.to_array(:hex)
+      end
+    end
   end
 end
 
--- app/models/url.rb
+++ app/models/url.rb
@@ -2,4 +2,26 @@ class Url < ActiveRecord::Base
   has_many :colors, through: :relationships
   has_many :relationships
 
+  def display_name
+    display=''
+
+    if self.url.match('http://www.')
+      display = self.url
+      display[0..10] = ''
+    elsif self.url.match('https://www.')
+      display = self.url
+      display[0..11] = ''
+    elsif self.url.match('http://')
+      display = self.url
+      display[0..6] = ''
+    elsif self.url.match('https://')
+      display = self.url
+      display[0..7] = ''
+    else
+      display = self.url
+    end
+
+    return display
+    
+  end
 end
